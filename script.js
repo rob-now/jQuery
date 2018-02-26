ed8d613cff1a4754def5a69bcd060f279f1ceef3
rob-now <rnowakowski.prv@gmail.com>
2018-02-26 13:15:46 +0100

    Selecting all and direct descendants, first, last, even, odd and multi selectors

--- script.js
+++ script.js
@@ -8,4 +8,18 @@ $(document).ready(function(){
     $("#list1");
     // Selecting class
     $(".best-price");
+    // Selecting all descendant li in list2 ID
+    $("#list2 li").text("all descendants");
+    // Selecting only direct descendant li in list2 ID
+    $("#list2 > li").text("direct descendants");
+    // Selecting only first li in list2 ID
+    $("#list2 li:first").text("li:first");
+    // Last
+    $("#list2 li:last").text("li:last");
+    // Odd (direct)
+    $("#list2 > li:odd").text("li:odd");
+    // Even (direct)
+    $("#list2 > li:even").text("li:even");
+    // Multi selectors
+    $("#list2 > li:first, .sub-list > li:first").text("li:first x 2");
 });
