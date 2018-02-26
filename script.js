8803526edf7c45c3b0e1f962d0e8659ace912351
rob-now <rnowakowski.prv@gmail.com>
2018-02-26 12:16:44 +0100

    Select span and change it's inner text using method text()

---
+++ script.js
@@ -0,0 +1,5 @@
+// Executing jQuery function after DOM ready/loaded
+$(document).ready(function(){
+    // Select span and change it's inner text using method text()
+    $("span").text("$25");
+});
