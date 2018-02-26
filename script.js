648739cd5f6ea56192396cd9df184976eebe6249
rob-now <rnowakowski.prv@gmail.com>
2018-02-26 12:33:18 +0100

    Selecting all h2, id and class

--- script.js
+++ script.js
@@ -2,4 +2,10 @@
 $(document).ready(function(){
     // Select span and change it's inner text using method text()
     $("span").text("$25");
+    // Select all h2 and change their titles
+    $("h2").text("Changed h2 titles");
+    // Selecting ID
+    $("#list1");
+    // Selecting class
+    $(".best-price");
 });
